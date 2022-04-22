7fcfbcbba4e154506d4c44bfbb101f7523bc0d96
log4x <achavezsa@unsa.edu.pe>
2022-04-21 19:39:04 -0500

    logController, it has errors

--- backend/src/controllers/logController.php
+++ backend/src/controllers/logController.php
@@ -16,26 +16,33 @@ class logController extends Controller{
     public function login($data){
         $UserName = $data['name'];
         $UserPass = $data['password'];
+        
         if(!is_null($UserName) && !is_null($UserPass)){
             if(User::exists($UserName)){
                 $user = User::get($UserName); 
                 if($user->comparePassword($UserPass)){
                     //guardarlo como toArray() el user de linea 21 guardarlo como un toArray
-                    $_SESSION['user'] = serialize($user);
+                    $_SESSION['user'] = serialize($user->toArray_2());
                     //direccionar correctamente generar respuestas generales
-                    $this->render('/home', ['op'=>0,'response'=>1,'message'=>'login successfully']);
+                    $this->render('/home', ['op'=>1,'response'=>1,'message'=>'login successfully','user'=>$_SESSION['user'] ]);
                 }else{
-                    $this->render('//home', ['op'=>0,'response'=>3,'message'=>'Wrong Password']);
+                    echo "contraseña incorrecta";
+                    //$this->render('/../Errors/home', ['op'=>0,'response'=>3,'message'=>'Wrong Password']);
                 }
             }else{
-                $this->render('/../TypeProduct/home', ['op'=>0,'response'=>2,'message'=>'User does not Exist']);
+                echo "usuario no existe";
+                //$this->render('/../Errors/home', ['op'=>0,'response'=>2,'message'=>'User does not Exist']);
             }    
         }else{
             echo "error";
         }
     }
     public function logout(){
-        session_destroy();
-        $this->render('/TypeProduct/home', ['op'=>0,'response'=>1,'message'=>'logout successfully']);
+        if(isset($_SESSION['user'])){
+            session_destroy();
+            $this->render('/home', ['op'=>2,'response'=>1,'message'=>'Logout successfully']);
+        }else{
+            $this->render('/home', ['op'=>2,'response'=>2,'message'=>'Usuario no inicio sesion previamente']);
+        }
     }
 }
